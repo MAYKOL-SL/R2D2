a80f05f03cc4b47de1d34ffc9a6fde852c4c5181
maykol JDSL <maykjdslsl@gmail.com>
2017-10-07 09:24:07 +0000

    cambios bostrap layot prinsipal funcionando y login modificado

--- reservaTis/resources/views/auth/login.blade.php
+++ reservaTis/resources/views/auth/login.blade.php
@@ -5,15 +5,15 @@
 @endsection
 
 @section('content')
-<body class="login-page">
-    <div class="login-box">
+<body class="fondo">
+    <div class="login-box ">
         <div class="login-logo">
-            <a href="{{ url('/home') }}"><b>Admin</b>LTE</a>
+            <a href="{{ url('/home') }}"><b>Reserva Ambiente</a>
         </div><!-- /.login-logo -->
 
     @if (count($errors) > 0)
         <div class="alert alert-danger">
-            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <strong>Hubo un error!</strong>Verifique los datos ingrasados .<br><br>
             <ul>
                 @foreach ($errors->all() as $error)
                     <li>{{ $error }}</li>
@@ -22,8 +22,8 @@
         </div>
     @endif
 
-    <div class="login-box-body">
-    <p class="login-box-msg">Sign in to start your session</p>
+    <div class="container card wizard-card">
+    <h2 class="texto">INICIAR SECION</h2>
     <form action="{{ url('/auth/login') }}" method="post">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <div class="form-group has-feedback">
@@ -35,15 +35,15 @@
             <span class="glyphicon glyphicon-lock form-control-feedback"></span>
         </div>
         <div class="row">
-            <div class="col-xs-8">
+            <div class="col-xs-4">
                 <div class="checkbox icheck">
                     <label>
-                        <input type="checkbox" name="remember"> Remember Me
+                        <input type="checkbox" name="remember">Recuerdame
                     </label>
                 </div>
             </div><!-- /.col -->
-            <div class="col-xs-4">
-                <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+            <div class="col-xs-8">
+                <button type="submit" class="btn btn-primary btn-block btn-lg">AUTENTICAR</button>
             </div><!-- /.col -->
         </div>
     </form>
@@ -54,8 +54,8 @@
         <a href="#" class="btn btn-block btn-social btn-google-plus btn-flat"><i class="fa fa-google-plus"></i> Sign in using Google+</a>
     </div><!-- /.social-auth-links -->
 
-    <a href="{{ url('/password/email') }}">I forgot my password</a><br>
-    <a href="{{ url('/auth/register') }}" class="text-center">Register a new membership</a>
+    <!--a href="{{ url('/password/email') }}">I forgot my password</a><br>
+    <a href="{{ url('/auth/register') }}" class="text-center">Register a new membership</a-->
 
 </div><!-- /.login-box-body -->
 
