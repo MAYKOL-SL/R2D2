d1ff69e5a0393b235af0eb6ad3470831872f2cb5
Noe Terrazas <noelito010@gmail.com>
2017-10-25 13:48:44 -0400

    modifique la tabla periodo

--- reservaTis/database/migrations/2017_10_04_003729_create_periodos_table.php
+++ reservaTis/database/migrations/2017_10_04_003729_create_periodos_table.php
@@ -14,6 +14,7 @@ class CreatePeriodosTable extends Migration
     {
         Schema::create('periodos', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('hora_ini',50);
             $table->string('hora',50);
             $table->timestamps();
 
