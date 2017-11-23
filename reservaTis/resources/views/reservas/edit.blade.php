0dbf7b8df5e0b9da014054aeb12139b97e330718
maykol JDSL <maykjdslsl@gmail.com>
2017-11-23 00:18:15 +0000

    solucionado errores de ambiente

--- reservaTis/resources/views/reservas/edit.blade.php
+++ reservaTis/resources/views/reservas/edit.blade.php
@@ -4,7 +4,7 @@
 @endsection
 
 @section('main-content')
- 
+
     <div class="box box-primary">
 
         <div class="box-header with-border">
@@ -14,11 +14,11 @@
         {!!Form::open(['route'=>['reservas.update',$reserva],'method'=>'PUT'])!!}
 
         <div class="box-header">
-            
-           
+
+
               {!!Form::label('Usuario')!!}
                 <select name="user_id" class="form-control" disabled>
-                    
+
                     @foreach ($user as $us)
                         @if($us->id==Auth::user()->id)
                         <option value="{{$us->id}}">
@@ -27,7 +27,7 @@
                         @endif
                     @endforeach
                 </select>
-         
+
 
         </div>
 
@@ -47,10 +47,10 @@
                             </option>
                             @endif
                     @endforeach
-                    
+
                 </select>
 
-               
+
             </div>
 
         </div>
@@ -66,26 +66,26 @@
             {!!Form::label('Descripcion Reserva')!!}
             {!!Form::text('description',$reserva->description,['class'=>'form-control','required'=>'required',])!!}
 
-            
+
         </div>
 
         <div class="box-header">
             {!!Form::label('Fecha inicio y final:')!!}
             <div class="input-group col-md-5">
-                        
+
                  <i class="fa fa-calendar col-md-1"></i>
                  <input type="date" name="fecha_ini" min={{$fechaActual}} class="col-md-5" value={{$reserva->start}} required>
-                                
+
                  <input type="date" name="fecha_fin" min={{$fechaActual}} class="col-md-5" value={{$reserva->end}} required>
-                                
-                    
+
+
             </div>
         </div>
-        
+
 
         <div class="box-header">
               {!!Form::label('Hora inicio y final:')!!}
-              <div class="input-group col-md-3"> 
+              <div class="input-group col-md-3">
               {!! Form::select('periodos[]',$hora,$horas,['class'=>'form-control input-sm select-tag','multiple','required']) !!}
               </div>
         </div>
@@ -93,33 +93,33 @@
                 <div class="box-header">
               {!!Form::label('Dias:')!!}
               <div class="input-group col-md-5">
-                      
+
                         <input type="checkbox" name="lunes" value="Lunes" checked="checked">Lunes<br>
-                      
+
                         <input type="checkbox" name="martes" value="Martes" checked="checked">Martes<br>
-                      
+
                         <input type="checkbox" name="miercoles" value="Miercoles" checked="checked">Miercoles<br>
-                      
+
                         <input type="checkbox" name="jueves" value="Jueves" checked="checked">Jueves<br>
-                      
+
                         <input type="checkbox" name="viernes" value="Viernes" checked="checked">Viernes<br>
                         <input type="checkbox" name="sabado" value="Sabado" checked="checked">Sábado<br>
                         <input type="checkbox" name="sabado" value="Domingo" checked="checked">Domingo<br>
-                      
+
               </div>
         </div>
 
         <div class="box-header">
               {!!Form::label('Hora inicio y final:')!!}
-              <div class="input-group col-md-3"> 
+              <div class="input-group col-md-3">
               {!! Form::select('periodos[]',$hora,$horas,['class'=>'form-control input-sm select-tag','multiple','required']) !!}
               </div>
         </div>
 
         <div class="box-header">
-            
+
             <td>
-                
+
                 {!!Form::submit('Editar Reserva',['class'=>'btn btn-primary'])!!}
 
             </td>
